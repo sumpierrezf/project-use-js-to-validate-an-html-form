b2adbfea517527a98e75b414e92671e152f46673
sumpierrezf <viqui09umpierrez@gmail.com>
2023-01-04 18:16:54 +0000

    Project use JS to validate an HTML form

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let form = document.querySelector("form");
+  form.addEventListener("submit", enviarDatos);
+
+  function enviarDatos(e) {
+    e.preventDefault();
+    let card = document.getElementById("card");
+    let cvc = document.getElementById("cvc");
+    let amount = document.getElementById("amount");
+    let firstName = document.getElementById("firstName");
+    let lastName = document.getElementById("lastName");
+    let city = document.getElementById("city");
+    let state = document.getElementById("state");
+    let postalCode = document.getElementById("postalCode");
+    let message = document.getElementById("message");
+    let alert = document.getElementById("alert");
+
+    console.log(card.value);
+    console.log(cvc.value);
+    console.log(amount.value);
+    console.log(firstName.value);
+    console.log(lastName.value);
+    console.log(city.value);
+    console.log(state.value);
+    console.log(postalCode.value);
+    console.log(message.value);
+    console.log(alert);
+
+    if (card.value === "") {
+      //console.log("El campo email está vacio");
+      document.getElementById("card").style.background = "red";
+      //alert
+      document.getElementById("alert");
+    } else {
+      document.getElementById("alert").style.visibility === "hidden";
+    }
+  }
 };
